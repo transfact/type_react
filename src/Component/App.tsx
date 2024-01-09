99cdb599501cf7cb0fc47419959a4ac258cc6ad6
transfact <transpaction@naver.com>
2024-01-10 02:02:06 +0900

    responsive

--- src/Component/App.tsx
+++ src/Component/App.tsx
@@ -28,13 +28,12 @@ class App extends React.Component{
                 Draw your Graphs<br/>
                 Effortlessly<br/> 
               </h1>
-              <p style={{fontSize : "15px", color : "gray", width :"400px"}}>
-                Graphify는 직관적이고 사용자 친화적인 인터페이스를 통해 간편하게 그래프를 그릴 수 있는 웹사이트입니다. <br/>데이터 시각화를 손쉽게 구현하고 확인하세요.
+              <p style={{fontSize : "15px", color : "gray", maxWidth :"400px", minWidth : "200px"}}>
+                Graphify는 테스트로 그래프를 그릴 수 있는 웹사이트입니다. 데이터 시각화를 손쉽게 구현하고 확인하세요.
               </p>
             </div>
             <div style={{margin : "5%"}}>
               <img
-                className="mx-auto aspect-[5/3] overflow-hidden rounded-xl object-cover"
                 height="300px"
                 src="/graph.jpg"
                 width="500px"
